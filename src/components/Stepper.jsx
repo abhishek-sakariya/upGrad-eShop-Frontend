e752da7c667d0f899dbc8684f7952380580703e9
abhishek-sakariya <abhishek.esd43@iiitb.net>
2023-03-26 23:37:46 +0530

    Upgrade eShop Application

---
+++ src/components/Stepper.jsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+export default function HorizontalStepper({
+  steps,
+  activeStep,
+  handleBack,
+  handleNext,
+  isStepSkipped,
+}) {
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Stepper activeStep={activeStep}>
+        {steps?.map((label, index) => {
+          const stepProps = {};
+          const labelProps = {};
+          if (isStepSkipped(index)) {
+            stepProps.completed = false;
+          }
+          return (
+            <Step key={label} {...stepProps}>
+              <StepLabel {...labelProps}>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+      {
+        
+      }
+    </Box>
+  );
+}
